559a978eaf5051f2789d0f7b1f6e278ba0478d16
Ifta Rodrigues <iftarodrigues@gmail.com>
2022-02-16 17:45:12 -0300

    add: validacao da senha do usuario

--- src/main/java/com/ifta/spring/Application.java
+++ src/main/java/com/ifta/spring/Application.java
@@ -3,7 +3,10 @@ package com.ifta.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
@@ -12,6 +15,12 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        BCryptPasswordEncoder encoder= new BCryptPasswordEncoder();
+        return encoder;
     }
 
 }
--- src/main/java/com/ifta/spring/controller/UserController.java
+++ src/main/java/com/ifta/spring/controller/UserController.java
@@ -2,18 +2,23 @@ package com.ifta.spring.controller;
 
 import com.ifta.spring.model.Usuario;
 import com.ifta.spring.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     private final UserRepository repository;
+    private final PasswordEncoder encoder;
 
-    public UserController(UserRepository repository) {
+    public UserController(UserRepository repository, PasswordEncoder encoder) {
         this.repository = repository;
+        this.encoder = encoder;
     }
 
     @GetMapping("/listAll")
@@ -23,6 +28,23 @@ public class UserController {
 
     @PostMapping("/save")
     public ResponseEntity<Usuario> save(@RequestBody Usuario usuario){
+        usuario.setPassword(encoder.encode(usuario.getPassword()));
         return ResponseEntity.ok(repository.save(usuario));
     }
+
+    @GetMapping("/validate")
+    public ResponseEntity<Boolean> validatePassword(@RequestParam String username, @RequestParam String password){
+        Optional<Usuario> optUser = repository.findByUsername(username);
+
+        if (optUser.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(false);
+        }
+
+        Usuario user= optUser.get();
+        boolean valid= encoder.matches(password, user.getPassword());
+
+        HttpStatus status = (valid) ? HttpStatus.OK : HttpStatus.UNAUTHORIZED;
+        return ResponseEntity.status(status).body(valid);
+    }
+
 }
